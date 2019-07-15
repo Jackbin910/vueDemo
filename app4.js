440000c5a1f07dafca5cc8ca319b00142d6f7d85
yangbin1 <yangbin199191@163.com>
2019-07-15 21:45:50 +0800

    222

---
+++ app4.js
@@ -0,0 +1,26 @@
+//实例化 vue对象
+new Vue({
+    el: "#vue-app",
+    data: {
+        a: 0,
+        b: 0,
+        age: 20
+    },
+    methods: {
+        /*addToA: function () {
+            return this.a + this.age;
+        },
+        addToB: function () {
+            return this.b + this.age;
+        }*/
+    },
+    computed: {
+        addToA: function () {
+            return this.a + this.age;
+        },
+        addToB: function () {
+            return this.b + this.age;
+        }
+    }
+
+});

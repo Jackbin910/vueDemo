d78882752e27b3efbd09f29fe3c616d6aacaa712
yangbin1 <yangbin199191@163.com>
2019-07-21 17:56:15 +0800

    222

--- app3.js
+++ app3.js
@@ -8,11 +8,11 @@ new Vue({
     methods:{
         logName:function () {
             //console.log("你正在输入名字");
-            this.name = this.$refs.name.value;
+            //this.name = this.$refs.name.value;
         },
         logAge:function () {
            // console.log("你正在输入年龄");
-            this.age = this.$refs.age.value;
+            //this.age = this.$refs.age.value;
         }
     }
 });

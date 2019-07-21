c90e09a5232b0df5d2f5e17806835428fd0b23c3
yangbin1 <yangbin199191@163.com>
2019-07-21 17:43:33 +0800

    222

---
+++ app9.js
@@ -0,0 +1,33 @@
+//实例化 vue对象
+var one = new Vue({
+    el: "#vue-app-one",
+    data: {
+        title:"app one"
+    },
+    methods: {},
+    computed: {
+        greet: function () {
+            return "hello app one";
+        }
+    }
+});
+
+//实例化 vue对象
+var two = new Vue({
+    el: "#vue-app-two",
+    data: {
+        title:"app two"
+    },
+    methods: {
+        changeTitle:function () {
+            one.title = "已经改名";
+        }
+    },
+    computed: {
+        greet: function () {
+            return "hello app two";
+        }
+    }
+});
+
+two.title = "app two 发生变化"
